8587fa3fa6076450075e804aa40bf81731d36475
Han Kang <hankang@people.cn>
2020-08-05 16:06:07 +0800

    补充暗黑模式的编辑状态效果图。

--- KenTagSelector/KenTagSelectorDemo/ViewController.m
+++ KenTagSelector/KenTagSelectorDemo/ViewController.m
@@ -24,8 +24,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    //已选栏目列表
     selectedTagArray = @[@"关注",@"头条",@"抗疫",@"北京",@"科技",@"汽车",@"社会",@"军事",@"知否",@"历史",@"要闻",@"财经",@"军事"].mutableCopy;
+    
+    //备选栏目列表
     otherTagArray = @[@"独家",@"航空",@"娱乐",@"影视",@"音乐",@"股票",@"体育",@"CBA",@"冬奥",@"手机",@"数码",@"房产",@"游戏",@"旅游",@"健康",@"亲子",@"时尚",@"艺术",@"星座",@"段子",@"跟帖",@"图片",@"萌宠",@"圈子"].mutableCopy;
+    
+    //固定栏目列表（可选），注意：这个列表里的文字必须在“已选栏目列表”中存在，否则设了也没有用
     residentArray = @[@"关注", @"头条"];
     
     NSMutableString *strSelected = [NSMutableString new];
@@ -39,13 +44,17 @@
 
 - (IBAction)onBtnSelectClicked:(id)sender {
     
+    //创建栏目选择器，输入已选栏目列表和备选栏目列表
     TagSelectorVC *selectorVC = [[TagSelectorVC alloc] initWithSelectedTags:selectedTagArray andOtherTags:otherTagArray];
+    
+    //设置固定栏目（可选步骤）
     selectorVC.residentTagStringArray = residentArray;
     
+    //弹出栏目选择界面
     [self presentViewController:selectorVC animated:YES completion:^{}];
     
     //返回所有选中栏目
-    __block  NSMutableString *_str = [NSMutableString new];
+    __block  NSMutableString *strChannels = [NSMutableString new];
     selectorVC.choosedTags = ^(NSArray *selectedTags, NSArray *otherTags) {
         self->selectedTagArray = @[].mutableCopy;
         self->otherTagArray = @[].mutableCopy;
@@ -54,16 +63,16 @@
         }
         for (Channel *channel in selectedTags) {
             [self->selectedTagArray addObject:channel.title];
-            [_str appendString:channel.title];
-            [_str appendString:@", "];
+            [strChannels appendString:channel.title];
+            [strChannels appendString:@", "];
         }
-        self->_labelSelected.text = _str;
+        self->_labelSelected.text = strChannels;
     };
     
     //用户点击了某个栏目的处理Block
     selectorVC.activeTag = ^(Channel *channel) {
-        [_str appendString:channel.title];
-        self->_labelSelected.text = _str;
+        [strChannels appendString:channel.title];
+        self->_labelSelected.text = strChannels;
     };
 }
 
